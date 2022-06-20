1a02e2b7f67a41a5e756894d4ea9657f4de64131
Jayshri <jayshri@codeandhue.com>
2022-06-20 08:02:12 +0530

    index and engineering service secrtions merged

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,6 +1,42 @@
-const body = document.querySelector("body");
-const navbar = document.querySelector(".main-header");
-const menuBtn = document.querySelector(".menu-btn");
+let triggersModal = document.querySelectorAll(".js-show-modal");
+let modals = document.querySelectorAll(".js-modal");
+
+function closeModal() {
+	modals.forEach((modal) => {
+		if (modal.classList.contains("is-open")) {
+			modal.classList.remove("is-open");
+		}
+	});
+}
+
+triggersModal.forEach((button) =>
+	button.addEventListener("click", function (e) {
+		e.preventDefault();
+		let modalID = this.dataset.modal;
+		console.log(modalID);
+		modals.forEach(function (modal) {
+			if (modal.dataset.modal == modalID) {
+				modal.classList.add("is-open");
+			}
+		});
+	})
+);
+
+modals.forEach((modal) => {
+	modal.addEventListener("click", function (event) {
+		const isOutside = !event.target.closest(".modal__inner");
+		const isCloseButton = event.target.matches(".js-close-modal");
+		if (isCloseButton || isOutside) {
+			closeModal();
+		}
+	});
+});
+
+window.addEventListener("keydown", (event) => {
+	if (event.key === "Escape") {
+		closeModal();
+	}
+});
 
 
 
@@ -41,24 +77,38 @@ const menuBtn = document.querySelector(".menu-btn");
   });
           
 
-  // $(document).ready(function () {
-  //   $("#menu-btn").click(function (e) { 
-  //     e.preventDefault();
-  //     $(".main-header").toggleClass("mobile-active");
-  //   });
+  $(document).ready(function () {
+    $("#menu-btn").click(function (e) { 
+      e.preventDefault();
+      $(".main-header").toggleClass("mobile-active");
+    });
 
 
-  //    // ------------------------scroll-down js starts------------------------
-  //    $('.scroll-down[data-target]').on('click', function (event) {
+     // ------------------------scroll-down js starts------------------------
+     $('.scroll-down[data-target]').on('click', function (event) {
 
-  //     var target = $(this.getAttribute('data-target'));
-  //     if (target.length) {
-  //         event.preventDefault();
-  //         $('html, body').stop().animate({
-  //             scrollTop: target.offset().top
-  //         }, 1000);
-  //     }
+      var target = $(this.getAttribute('data-target'));
+      if (target.length) {
+          event.preventDefault();
+          $('html, body').stop().animate({
+              scrollTop: target.offset().top
+          }, 1000);
+      }
 
-  // });
-  // // ------------------------scroll-down js ends--------------------------
-  // });
+  });
+  // ------------------------scroll-down js ends--------------------------
+
+   // ------------------------scroll-up js starts------------------------
+   $('.scroll-top[data-target]').on('click', function (event) {
+
+    var target = $(this.getAttribute('data-target'));
+    if (target.length) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+
+});
+// ------------------------scroll-down js ends--------------------------
+  });
